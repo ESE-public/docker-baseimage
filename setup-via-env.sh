db0a1a42271c6ffcc3554ce7490dfd0a6d35819d
Keith Bentrup <keith@keithbentrup.com>
2015-09-29 19:24:48 -0400

    run script in background

--- setup-via-env.sh
+++ setup-via-env.sh
@@ -2,7 +2,7 @@
 
 if [ "${SETUP_SCRIPT}" != "" ]; then
   if [ -x "${SETUP_SCRIPT}" ]; then
-     exec "${SETUP_SCRIPT}"
+     exec "${SETUP_SCRIPT}" &
   else 
     echo "${SETUP_SCRIPT} is not an executable file" && exit 1 
   fi
