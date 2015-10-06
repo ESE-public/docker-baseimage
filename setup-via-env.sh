608d7d7510d32b39d75b1794908da73997bb7471
Keith Bentrup <keith@keithbentrup.com>
2015-10-06 09:32:04 -0400

    just echo warning but do not exit with nonzero status

--- setup-via-env.sh
+++ setup-via-env.sh
@@ -4,6 +4,6 @@ if [ "${SETUP_SCRIPT}" != "" ]; then
   if [ -x "${SETUP_SCRIPT}" ]; then
      exec "${SETUP_SCRIPT}" &
   else 
-    echo "${SETUP_SCRIPT} is not an executable file" && exit 1 
+    echo "${SETUP_SCRIPT} is not an executable file" 
   fi
 fi
